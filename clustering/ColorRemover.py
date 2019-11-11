e64ea8921418948e7e08ac8a468520c4e45c19cd
Dalton Omens <Dalton@DESKTOP-7G66B4S.(none)>
2019-11-10 20:32:46 -0800

    numpy god

--- clustering/ColorRemover.py
+++ clustering/ColorRemover.py
@@ -8,34 +8,44 @@ import matplotlib.pyplot as plt
 
 
 def get_image_colors(im):
-	numpy_im = np.array(im)
-	# colors = resize(numpy_im, (32, 32), anti_aliasing=True)
-	if colors.dtype == np.uint8:
-		colors = colors.astype(np.float64) / 255
-	return colors
+    numpy_im = np.array(im)
+    # colors = resize(numpy_im, (32, 32), anti_aliasing=True)
+    if colors.dtype == np.uint8:
+        colors = colors.astype(np.float64) / 255
+    return colors
 
 def get_cluster_labels(image, num_clusters):
-	colors = get_image_colors(image)
-	original_shape = colors.shape
-	colors = colors.reshape((colors.shape[0] * colors.shape[1], 3))
-	cluster_labels = cluster_colors(colors, num_clusters)
-	cluster_labels = cluster_labels.reshape((original_shape[0], original_shape[1]))
-	return cluster_labels
+    colors = get_image_colors(image)
+    original_shape = colors.shape
+    colors = colors.reshape((colors.shape[0] * colors.shape[1], 3))
+    cluster_labels = cluster_colors(colors, num_clusters)
+    cluster_labels = cluster_labels.reshape((original_shape[0], original_shape[1]))
+    return cluster_labels
+
+def remove_colors(image, labels, to_remove):
+    num = np.max(labels)
+    new_image = image.copy()
+    labels_sorted = np.asarray(to_remove + [x for x in labels if x not in np.arange(10)])
+    labels_map = np.argsort(labels_sorted - 1)
+    new_labels = labels_map[labels - 1]
+    new_image[new_labels < len(to_remove)] = 0.
+    return new_image
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--image_dir", type=str)
-	parser.add_argument("--num_clusters", type=int, default=10)
-	parser.add_argument("--rescale_size", type=int, default=128)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_dir", type=str)
+    parser.add_argument("--num_clusters", type=int, default=10)
+    parser.add_argument("--rescale_size", type=int, default=128)
+    args = parser.parse_args()
 
-	image_dir = args.image_dir
-	num_clusters = args.num_clusters
+    image_dir = args.image_dir
+    num_clusters = args.num_clusters
 
-	images = load_images(image_dir)
-	for image in images:
-		get_cluster_labels(image, num_clusters)
+    images = load_images(image_dir)
+    for image in images:
+        cluster_labels = get_cluster_labels(image, num_clusters)
+        
 
 
 if __name__ == "__main__":
-	main()
+    main()
